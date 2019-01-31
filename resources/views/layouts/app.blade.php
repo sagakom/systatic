fa5ca8513be260ddd17bcdfdef508ff5ede8714a
Duncan McClean <duncan@mcclean.co.uk>
2019-01-31 21:22:06 +0000

    Layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@title() - @siteName()</title>
+</head>
+<body>
+    <div id="app">
+        @include('partials.header')
+    
+        @yield('content')
+    
+        @include('partials.footer')
+    </div>
+
+    <script src="/js/app.js"></script>
+</body>
+</html>
