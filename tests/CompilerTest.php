1d9e3091ebae55e1e4dca078c73e71ee1cd75399
Duncan McClean <duncan@mcclean.co.uk>
2019-03-31 20:27:31 +0100

    Markdown compiler tests

---
+++ tests/CompilerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Thunderbird\Compiler\Compiler;
+
+class CompilerTest extends TestCase
+{
+    public function testCanCompileMarkdownWithFrontMatter()
+    {  
+        $compiler = new Compiler();
+        $compile = $compiler->compile('./content/tests/markdown_with_frontmatter.md');
+        $this->assertSame(true, $compile);
+        $this->assertFileExists('dist/markdown_with_frontmatter.html');
+    }
+
+    public function testCanCompileMarkdownWithoutFrontmatter()
+    {
+        $compiler = new Compiler();
+        $compile = $compiler->compile('./content/tests/markdown_without_frontmatter.md');
+        $this->assertSame(true, $compile);
+        $this->assertFileExists('dist/markdown_without_frontmatter.html');
+    }
+
+    public function testCanCompileMarkdownWithHtmlCodeInside()
+    {
+        $compiler = new Compiler();
+        $compile = $compiler->compile('./content/tests/markdown_with_html_inside.md');
+        $html = strpos($compile, '<span class="awesome"><strong>Awesome</strong></span>');
+        $this->assertSame(true, $compile);
+        $this->assertEquals($html, 0);
+        $this->assertFileExists('dist/markdown_with_html_inside.html');
+    }
+}
