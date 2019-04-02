a220d4f56601bbe5af88e1f499e69048bfea451f
Duncan McClean <duncan@mcclean.co.uk>
2019-04-02 10:50:28 +0100

    Seperated Blade compiler into its own file

---
+++ src/Compiler/BladeCompiler.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Thunderbird\Compiler;
+
+use Thunderbird\Cache\Cache;
+use Thunderbird\Config\Config;
+use Jenssegers\Blade\Blade;
+
+class BladeCompiler
+{
+    public function __construct()
+    {
+        $this->cache = new Cache();
+        $this->config = new Config();
+        $this->blade = new Blade($this->config->getConfig('viewsDir'), $this->config->getConfig('cacheDir'));
+    }
+
+    public function compile($template, $slug, $title, $content, $matter)
+    {
+        // Make the page with the chosen blade template and with all the variables
+        $page = $this->blade->make($template, [
+            'title' => $title,
+            'content' => $content
+        ]);
+
+        // Setup a config variable for using it in blade
+        $config = $this->config;
+
+        // Directive: Front Matter Variable
+        $this->blade->compiler()->directive('matter', function($variable) use($matter)
+        {
+            if (array_key_exists($variable, $matter)) {
+                return $matter[$variable];
+            }
+        });
+
+        // Directive: Site Name
+        $this->blade->compiler()->directive('siteName', function() use($config) 
+        {
+            return $config->getConfig('siteName');
+        });
+
+        // Directive: Site URL
+        $this->blade->compiler()->directive('siteUrl', function() use($config)
+        {
+            return $config->getConfig('siteUrl');
+        });
+
+        // Directive: Config Value
+        $this->blade->compiler()->directive('config', function($setting) use($config) 
+        {
+            return $config->getConfig($setting);
+        });
+
+        // Directive: Env Value
+        $this->blade->compiler()->directive('env', function($setting) use($config) 
+        {
+            return $config->getEnv($setting);
+        });
+
+        // Output the final blade template
+        file_put_contents($this->config->getConfig('outputDir') . '/' . $slug . '.html', $page);
+
+        // Clear cache
+        $this->cache->clearCache();
+
+        return true;
+    }
+}
--- src/Compiler/Compiler.php
+++ src/Compiler/Compiler.php
@@ -3,11 +3,11 @@
 namespace Thunderbird\Compiler;
 
 use Damcclean\Markdown\MetaParsedown;
-use Jenssegers\Blade\Blade;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Thunderbird\Config\Config;
 use Thunderbird\Cache\Cache;
+use Thunderbird\Compiler\BladeCompiler;
 
 class Compiler 
 {
@@ -16,7 +16,7 @@ class Compiler
         $this->config = new Config();
         $this->cache = new Cache();
         $this->parsedown = new MetaParsedown();
-        $this->blade = new Blade($this->config->getConfig('viewsDir'), $this->config->getConfig('cacheDir'));
+        $this->blade = new BladeCompiler();
         $this->filesystem = new Filesystem();
     }
 
@@ -55,52 +55,8 @@ class Compiler
             $template = $slug;
         }
 
-        // Make the page with the chosen blade template and with all the variables
-        $page = $this->blade->make($template, [
-            'title' => $title,
-            'content' => $markdown
-        ]);
-
-        // Setup a config variable for using it in blade
-        $config = $this->config;
-
-        // Directive: Front Matter Variable
-        $this->blade->compiler()->directive('matter', function($variable) use($matter)
-        {
-            if (array_key_exists($variable, $matter)) {
-                return $matter[$variable];
-            }
-        });
-
-        // Directive: Site Name
-        $this->blade->compiler()->directive('siteName', function() use($config) 
-        {
-            return $config->getConfig('siteName');
-        });
-
-        // Directive: Site URL
-        $this->blade->compiler()->directive('siteUrl', function() use($config) 
-        {
-            return $config->getConfig('siteUrl');
-        });
-
-        // Directive: Config Value
-        $this->blade->compiler()->directive('config', function($setting) use($config) 
-        {
-            return $config->getConfig($setting);
-        });
-
-        // Directive: Env Value
-        $this->blade->compiler()->directive('env', function($setting) use($config) 
-        {
-            return $config->getEnv($setting);
-        });
-
-        // Output the final blade template
-        file_put_contents($this->config->getConfig('outputDir') . '/' . $slug . '.html', $page);
-
-        // Clear cache
-        $this->cache->clearCache();
+        // Compile 
+        $this->blade->compile($template, $slug, $title, $markdown, $matter);
 
         return true;
     }
