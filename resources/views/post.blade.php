7b927b4c8b59f0e890f2e05f9c334a9e5b217fbf
Duncan McClean <duncan@mcclean.co.uk>
2019-04-03 14:13:34 +0100

    Created stubs

--- resources/views/post.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container mx-auto">
-    <h1>{{ $title }}</h1>
-    <h3>Published on @matter(date) by @matter(author).</h3>
-    {!! $content !!}
-</div>
-@endsection
