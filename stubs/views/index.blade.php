d4dcf2df8378bcbb5ddffcd0aafec37a46ad6c96
Duncan McClean <duncan@mcclean.co.uk>
2019-05-19 17:37:20 +0100

    More useful index page

---
+++ stubs/views/index.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Systatic</title>
+		<meta charset="UTF-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<style>
+			body {
+			  margin: 0px;
+			  padding: 0px;
+			  background-color: #ffb305;
+			  font-weight: 10;
+			  color: #ffffff;
+			  font-family: 'Roboto';
+			}
+
+			div {
+			  display: flex;
+			  flex-direction: column;
+			  align-items: center;
+			  justify-content: center;
+			  height: 100vh;
+			}
+
+			h1 {
+			  font-size: 80px;
+			  line-height: 1;
+			}
+
+			nav {
+			  display: flec;
+			  flex-direction: row;
+			  align-items: center;
+			}
+
+			a {
+			  margin-left: 5px;
+			  margin-right: 5px;
+			  color: #ffffff;
+			  text-decoration: none;
+			  font-size: 17px;
+			}
+		</style>
+	</head>
+	<body>
+	  <div>
+	    <h1>Systatic</h1>
+	    <nav>
+	      <a href="https://systatic.netlify.com/docs">Documentation</a>
+	      <a href="https://github.com/damcclean/systatic">Github</a>
+	      <a href="https://discord.gg/sxkrycQ">Discord</a>
+	    </nav>
+	  </div>
+	</body>
+</html>
