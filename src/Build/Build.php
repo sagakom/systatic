d1d6aa65339574b0f13c3b7df77e2effa2907095
Duncan McClean <duncan@mcclean.co.uk>
2019-05-24 18:07:24 +0100

    Added a little bit of commenting

--- src/Build/Build.php
+++ src/Build/Build.php
@@ -15,37 +15,31 @@ class Build
         $this->compiler = new Compiler();
     }
 
+    /*
+        Site Build
+    */
     public function build()
     {
-        // Create an empty array to store markdown files
-        $markdown = array();
+        $markdown = [];
 
-        // Get files to send to compiler
         $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*.md', GLOB_BRACE), $markdown);
         $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*/*.md', GLOB_BRACE), $markdown);
         $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*.markdown', GLOB_BRACE), $markdown);
         $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*/*.markdown', GLOB_BRACE), $markdown);
 
-        // Compile each of the files
         foreach ($markdown as $file) {
             $this->compiler->markdown($file);
         }
 
-        // Create an empty array to store html files
-        $html = array();
+        $html = [];
 
-        // Get files to send to compiler
         $html = array_merge(glob($this->config->getConfig('contentDir') . '/*.html', GLOB_BRACE), $html);
         $html = array_merge(glob($this->config->getConfig('contentDir') . '/*/*.html', GLOB_BRACE), $html);
 
-        // Compile each of the files
         foreach ($html as $file) {
             $this->compiler->html($file);
         }
 
-        // Build the redirects
         $this->redirects->build();
-
-        return true;
     }
 }
