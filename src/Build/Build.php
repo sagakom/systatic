3635c51c2e4dae0ddb642c3e3312c227e1f4d72f
Duncan McClean <duncan@mcclean.co.uk>
2019-05-25 17:50:40 +0100

    Changed the build method from using the old compiler to sending it to the collections stuff

--- src/Build/Build.php
+++ src/Build/Build.php
@@ -3,16 +3,14 @@
 namespace Damcclean\Systatic\Build;
 
 use Damcclean\Systatic\Build\Redirects;
-use Damcclean\Systatic\Compiler\Compiler;
-use Damcclean\Systatic\Config\Config;
+use Damcclean\Systatic\Collections\Collections;
 
 class Build
 {
     public function __construct()
     {
+        $this->collections = new Collections();
         $this->redirects = new Redirects();
-        $this->config = new Config();
-        $this->compiler = new Compiler();
     }
 
     /*
@@ -21,26 +19,7 @@ class Build
     
     public function build()
     {
-        $markdown = [];
-
-        $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*.md', GLOB_BRACE), $markdown);
-        $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*/*.md', GLOB_BRACE), $markdown);
-        $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*.markdown', GLOB_BRACE), $markdown);
-        $markdown = array_merge(glob($this->config->getConfig('contentDir') . '/*/*.markdown', GLOB_BRACE), $markdown);
-
-        foreach ($markdown as $file) {
-            $this->compiler->markdown($file);
-        }
-
-        $html = [];
-
-        $html = array_merge(glob($this->config->getConfig('contentDir') . '/*.html', GLOB_BRACE), $html);
-        $html = array_merge(glob($this->config->getConfig('contentDir') . '/*/*.html', GLOB_BRACE), $html);
-
-        foreach ($html as $file) {
-            $this->compiler->html($file);
-        }
-
+        $this->collections->collect();
         $this->redirects->build();
     }
 }
