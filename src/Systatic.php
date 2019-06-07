6c460cb8117950fa14042006b9fec3899a407c38
Duncan McClean <duncan@mcclean.co.uk>
2019-06-07 10:52:54 +0100

    bootstrap the application

--- src/Systatic.php
+++ src/Systatic.php
@@ -2,7 +2,17 @@
 
 namespace Damcclean\Systatic;
 
+use Damcclean\Systatic\Commands\Commands;
+
 class Systatic
 {
-    //
+    public function boot()
+    {
+        /*
+            Commands
+            - Register console commands
+        */
+
+        (new Commands())->console();
+    }
 }
