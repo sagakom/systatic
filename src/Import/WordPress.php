3db9c339f3cecf2b13777d9dd3f49b6214c1d235
Duncan McClean <duncan@mcclean.co.uk>
2019-06-16 18:09:40 +0100

    added posts and fixed wordpress import errors

--- src/Import/WordPress.php
+++ src/Import/WordPress.php
@@ -2,6 +2,7 @@
 
 namespace Damcclean\Systatic\Import;
 
+use Symfony\Component\Yaml\Yaml;
 use Damcclean\Systatic\Build\Build;
 use Damcclean\Systatic\Config\Config;
 
@@ -18,13 +19,18 @@ class WordPress
         $apiUrl = $baseUrl . '/wp-json/wp/v2';
 
         $pages = json_decode(@file_get_contents($apiUrl . '/pages'), true);
+        $posts = json_decode(@file_get_contents($apiUrl . '/posts'), true);
 
-        if(!$pages) {
+        if(!$pages || !$posts) {
             return false;
         }
 
         foreach($pages as $page) {
             $this->createFile($page);
+        }
+
+        foreach($posts as $post) {
+            $this->createFile($post);
         }
 
         $this->build->build();
@@ -34,17 +40,16 @@ class WordPress
 
     public function createFile($page)
     {
-        $contents = 
-                '---' .
-                ' title: "' . $page['title']['rendered'] . '"' .
-                ' slug: "' . $page['slug'] . '"' .
-                ' excerpt: "' . $page['excerpt']['rendered'] . '"' .
-                ' date: "' . $page['date'] . '"' .
-                '---' .
-                $page['content']['rendered'];
+        $frontMatter = [
+            'title' => $page['title']['rendered'],
+            'slug' => $page['slug'],
+            'excerpt' => $page['excerpt']['rendered'],
+            'date' => $page['date']
+        ];
 
-            $filename = $this->config->get('locations.content') . '/' . $page['slug'] . '.md';
+        $contents = '---' . Yaml::dump($frontMatter) . '---' . $page['content']['rendered'];
+        $filename = $this->config->get('locations.content') . '/' . $page['slug'] . '.md';
 
-            file_put_contents($filename, $contents);
+        file_put_contents($filename, $contents);
     }
 }
