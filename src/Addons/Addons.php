e944cc01a158588536a975cde62ed444b15a5942
Duncan McClean <duncan@mcclean.co.uk>
2019-06-17 18:45:25 +0100

    Plugins ecosystem

---
+++ src/Addons/Addons.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Damcclean\Systatic\Addons;
+
+use Damcclean\Systatic\Config\Config;
+
+class Addons
+{
+    public function __construct()
+    {
+        $this->plugins = [];
+
+        $this->config = new Config();
+    }
+
+    public function find()
+    {
+        $location = './plugins';
+
+        if(array_key_exists('plugins', $this->config->getArray()['locations'])) {
+            $location = $this->config->get('locations.plugins');
+        }
+
+        $this->plugins = array_merge(glob($location . '/*.php', GLOB_BRACE), $this->plugins);
+    }
+
+    public function beforeBuild()
+    {
+        $this->find();
+
+        foreach($this->plugins as $plugin) {
+            $include = include $plugin;
+            before();
+        }
+    }
+
+    public function afterBuild()
+    {
+        $this->find();
+
+        foreach($this->plugins as $plugin) {
+            $include = include $plugin;
+            after();
+        }
+    }
+}
