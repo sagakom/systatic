4ff4a9715cf270692c3c03385195a7a2bab18569
Duncan McClean <duncan@mcclean.co.uk>
2019-07-06 20:23:14 +0100

    remove construct

--- src/Collections/Remote.php
+++ src/Collections/Remote.php
@@ -4,11 +4,6 @@ namespace Damcclean\Systatic\Collections;
 
 class Remote
 {
-    public function __construct()
-    {
-        //
-    }
-
     public function parse($entry, $collection)
     {
         // Do the basic adding to the collection
