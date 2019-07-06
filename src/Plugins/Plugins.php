e66392a05c4d92e67647c34203e160551e2d0a5a
Duncan McClean <duncan@mcclean.co.uk>
2019-07-06 11:42:31 +0100

    wip

---
+++ src/Plugins/Plugins.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Damcclean\Systatic\Plugins;
+
+use Damcclean\Systatic\Config\Config;
+
+class Plugins
+{
+    public function __construct()
+    {
+        $this->config = new Config();
+
+        $this->store = [];
+    }
+
+    public function save($store)
+    {
+        file_put_contents($this->config->get('locations.storage') . '/plugins.json', json_encode($store));
+        return true;
+    }
+
+    public function fetch()
+    {
+        return json_decode(file_get_contents($this->config->get('locations.storage') . '/plugins.json'), true);
+    }
+
+    public function register()
+    {
+        foreach($this->config->getArray()['plugins'] as $plugin) {
+            $p = new $plugin();
+
+            array_push($p, $this->store);
+        }
+
+        $this->save($this->store);
+    }
+}
--- src/Systatic.php
+++ src/Systatic.php
@@ -2,17 +2,14 @@
 
 namespace Damcclean\Systatic;
 
+use Damcclean\Systatic\Plugins\Plugins;
 use Damcclean\Systatic\Commands\Commands;
 
 class Systatic
 {
     public function boot()
     {
-        /*
-            Commands
-            - Register console commands
-        */
-
+        (new Plugins())->register();
         (new Commands())->console();
     }
 }
