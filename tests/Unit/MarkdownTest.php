65a6ef9bd65b413b55289ff02307210a640c0a18
Duncan McClean <duncan@mcclean.co.uk>
2019-07-12 21:02:08 +0100

    wip

---
+++ tests/Unit/MarkdownTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests;
+
+use Damcclean\Systatic\Collections\Markdown;
+use Damcclean\Systatic\Config\Config;
+use Damcclean\Systatic\RefreshSite;
+use Tests\TestCase;
+
+class MarkdownTest extends TestCase
+{
+    use RefreshSite;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->config = new Config();
+        $this->markdown = new Markdown();
+    }
+
+    public function testCanParseMarkdown()
+    {
+        $file = './tests/fixtures/content/pages/contact-me.md';
+        $collection = $this->config->getArray()['collections']['pages'];
+        $collection['key'] = 'pages';
+
+        $markdown = $this->markdown->parse($file, $collection);
+
+        $this->assertIsArray($markdown);
+        $this->assertSame($markdown['title'], 'Contact Me');
+        $this->assertSame($markdown['slug'], 'contact');
+        $this->assertSame($markdown['view'], 'contact');
+        $this->assertStringContainsString('<p>', $markdown['content']);
+    }
+}
