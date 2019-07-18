9c99067ac85a13f5eabc6304edac03d44bc026ba
Duncan McClean <duncan@mcclean.co.uk>
2019-07-18 20:27:58 +0100

    assert that meta is object

--- tests/Unit/PageTest.php
+++ tests/Unit/PageTest.php
@@ -54,4 +54,24 @@ class PageTest extends TestCase
 
         $this->assertIsObject($page['config']);
     }
+
+    public function testMetaIsObject()
+    {
+        $data = [
+            'filename' => './tests/fixtures/content/pages/index.md',
+            'permalink' => '/index.html',
+            'title' => 'Homepage',
+            'slug' => 'index',
+            'view' => 'index',
+            'content' => '<p>This is the homepage of my wonderful website.</p>',
+            'meta' => [
+                'title' => 'Homepage'
+            ],
+            'last_updated' => null
+        ];
+
+        $page = $this->page->process($data);
+
+        $this->assertIsObject($page['meta']);
+    }
 }
